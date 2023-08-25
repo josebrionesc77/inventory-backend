7b1ed3acaba652507d0b10ed2dd1315f02fad30e
josebrionesc77 <josebrionesc77@outlook.com>
2023-08-25 17:05:31 -0500

    Implementacion del servicio de guardado de categorias

--- src/main/java/com/briosys/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/briosys/inventory/services/CategoryServiceImpl.java
@@ -75,4 +75,34 @@ public class CategoryServiceImpl implements ICategoryService {
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try {
+			
+			Category categorySaved = categoryDao.save(category);
+			if(categorySaved != null)
+			{
+				list.add(categorySaved);
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Categoria Guardada exitosamente");
+			}
+			else
+			{
+				response.setMetadata("Respuesta not ok", "-1", "Categoria no Guardada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}			
+		
+		}catch (Exception e) {
+
+			response.setMetadata("Respuesta not ok", "-1", "Error al guardar la categoria");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
 }
--- src/main/java/com/briosys/inventory/services/ICategoryService.java
+++ src/main/java/com/briosys/inventory/services/ICategoryService.java
@@ -1,6 +1,8 @@
 package com.briosys.inventory.services;
 
 import org.springframework.http.ResponseEntity;
+
+import com.briosys.inventory.model.Category;
 import com.briosys.inventory.response.*;
 
 public interface ICategoryService {
@@ -8,5 +10,7 @@ public interface ICategoryService {
 	public ResponseEntity<CategoryResponseRest> search();
 	
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
