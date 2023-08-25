fb3c1d6683c9153859a7612f09003012c0597cd1
josebrionesc77 <josebrionesc77@outlook.com>
2023-08-25 16:40:19 -0500

    Implementacion del servicio de consulta de categorias por id

--- src/main/java/com/briosys/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/briosys/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.briosys.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -40,6 +42,37 @@ public class CategoryServiceImpl implements ICategoryService {
 		}
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try {
+			
+			Optional<Category> category = categoryDao.findById(id);
+			if(category.isPresent())
+			{
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Categoria Encontrada");
+			}
+			else
+			{
+				response.setMetadata("Respuesta not ok", "-1", "Categoria no Encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+		
+		}catch (Exception e) {
+
+			response.setMetadata("Respuesta not ok", "-1", "Error al consultar por Id");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
 }
